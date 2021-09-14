60f4c27ff8b27b74ed862630329d8e69bf5a7d82
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-09-14 20:45:57 +0300

    All done.

--- contacts.js
+++ contacts.js
@@ -1,4 +1,64 @@
 const fs = require("fs/promises");
 const path = require("path");
+const { v4 } = require("uuid");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+const listContacts = async () => {
+  const allContacts = await fs.readFile(contactsPath, "utf-8");
+
+  return JSON.parse(allContacts);
+};
+
+const getContactById = async (contactId) => {
+  const allContacts = await listContacts();
+  const contact = allContacts.find(item => item.id === contactId);
+
+  if (!contact) {
+    throw new Error("Contact not found.")
+  };
+
+  return contact;
+};
+
+const removeContact = async (contactId) => {
+  const allContacts = await listContacts();
+  const idx = allContacts.findIndex(item => item.id === contactId);
+
+  if (idx < 0) {
+    throw new Error("Contact not found.")
+  };
+
+  const deletedContact = await getContactById(contactId);
+
+  allContacts.splice(idx, 1);
+  await updateContacts(allContacts);
+
+  return deletedContact;
+};
+
+const addContact = async (name, email, phone) => {
+  const allContacts = await listContacts();
+  const newContact = {
+    id: v4(),
+    name,
+    email,
+    phone,
+  };
+
+  allContacts.push(newContact);
+  await updateContacts(allContacts);
+
+  return newContact;
+};
+
+const updateContacts = async (newContacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("Hello");
+const contacts = require("./contacts");
+const argv = require('yargs').argv;
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  try {
+    switch (action) {
+      case 'list':
+        const allContacts = await contacts.listContacts();
+        console.table(allContacts);
+        break;
+
+      case 'get':
+        const contact = await contacts.getContactById(id);
+        console.table(contact);
+        break;
+
+      case 'add':
+        console.log("\nAdding a contact...");
+
+        console.table(await contacts.addContact(name, email, phone,));
+        console.table(await contacts.listContacts());
+        console.log("The contact has been successfully added");
+        break;
+
+      case 'remove':
+        console.log("\nRemoveing a contact...");
+        console.table(await contacts.getContactById(id));
+
+        await contacts.removeContact(id);
+
+        console.table(await contacts.listContacts());
+        console.log("The contact has been successfully removed");
+        break;
+
+      default:
+        console.warn('\x1B[31m Unknown action type!');
+    }
+  } catch (error) {
+    console.log(error.message);
+  };
+};
+
+invokeAction(argv);
