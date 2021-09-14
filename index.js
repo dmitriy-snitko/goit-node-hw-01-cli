81752d98e358ff7fe1555e09d910699e9b3f4e09
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-09-14 23:22:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
         console.log("\nAdding a contact...");
 
         console.table(await contacts.addContact(name, email, phone,));
+        console.log("\nThe contact has been successfully added");
+
         console.table(await contacts.listContacts());
-        console.log("The contact has been successfully added");
         break;
 
       case 'remove':
@@ -27,9 +28,9 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
         console.table(await contacts.getContactById(id));
 
         await contacts.removeContact(id);
+        console.log("\nThe contact has been successfully removed");
 
         console.table(await contacts.listContacts());
-        console.log("The contact has been successfully removed");
         break;
 
       default:
